eed328192a59300c2dad0bea4006b54e8c2576f0
taqiussa <taqiussa@gmail.com>
2023-05-03 18:43:49 +0700

    print daftar nilai

--- resources/views/print/guru/print-daftar-nilai.blade.php
+++ resources/views/print/guru/print-daftar-nilai.blade.php
@@ -25,7 +25,7 @@
             </div>
         </div>
     </div>
-    <table class="w-full pt-3">
+    <table class="w-full pt-3 text-xs">
         <thead>
             <tr>
                 <th class="border border-collapse border-black">No</th>
@@ -37,5 +37,39 @@
                 <th class="border border-collapse border-black">Nilai Rapor</th>
             </tr>
         </thead>
+        <tbody>
+            @foreach ($listSiswa as $siswa)
+                <tr>
+                    <td class="border border-collapse border-black text-center px-1">{{ $loop->iteration }}</td>
+                    <td class="border border-collapse border-black text-center px-1">{{ $siswa->nis }}</td>
+                    <td class="border border-collapse border-black px-2">{{ $siswa->user?->name }}</td>
+                    @foreach ($listJenis as $jenis)
+                        <td class="border border-collapse border-black text-center px-1">
+                            @foreach ($siswa->penilaians as $nilai)
+                                @if ($nilai->jenis_penilaian_id == $jenis->id)
+                                    {{ $nilai->nilai }}
+                                @endif
+                            @endforeach
+
+                        </td>
+                    @endforeach
+                    <td class="border border-collapse border-black text-center px-1">
+                        {{ floor($siswa->penilaians->avg('nilai')) }}
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
     </table>
+    <div class="flex justify-between items-start text-sm pt-3 px-10">
+        <div class="flex flex-col items-center justify-center">
+            <div>&nbsp;</div>
+            <div class="font-semibold">Wali Kelas</div>
+            <div class="font-bold pt-16">{{ $namaWaliKelas }}</div>
+        </div>
+        <div class="flex flex-col items-center justify-center">
+            <div class="font-semibold">Ngampel, {{ tanggal(date('Y-m-d')) }}</div>
+            <div class="font-semibold">Guru Mata Pelajaran</div>
+            <div class="font-bold pt-16">{{ auth()->user()->name }}</div>
+        </div>
+    </div>
 @endsection

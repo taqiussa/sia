a237c57b41748b2e7af60c4d595b59b1b769a67c
taqiussa <taqiussa@gmail.com>
2023-05-05 23:52:35 +0700

    print ledger rapor

--- app/Http/Controllers/PrintLedgerRaporController.php
+++ app/Http/Controllers/PrintLedgerRaporController.php
@@ -2,9 +2,47 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Kelas;
+use App\Models\PenilaianRapor;
+use App\Traits\InitTrait;
+use App\Traits\SiswaTrait;
 
 class PrintLedgerRaporController extends Controller
 {
-    //
+    use InitTrait;
+    use SiswaTrait;
+
+    public function index()
+    {
+        return inertia(
+            'Guru/PrintLedgerRapor',
+            [
+                'initTahun' => $this->data_tahun(),
+                'initSemester' => $this->data_semester(),
+                'listKelas' => Kelas::orderBy('nama')->get(),
+            ]
+        );
+    }
+
+    public function print()
+    {
+        $kelas = Kelas::find(request('kelasId'));
+
+        $listJenis = PenilaianRapor::whereTahun(request('tahun'))
+            ->whereSemester(request('semester'))
+            ->whereTingkat($kelas->tingkat)
+            ->pluck('jenis_penilaian_id');
+
+        $data = [
+            'namaKelas' => $kelas->nama,
+            'tahun' => request('tahun'),
+            'semester' => request('semester'),
+            'namaWaliKelas' => $this->data_nama_wali_kelas(),
+            'listSiswa' => $this->data_siswa_with_penilaian_rapors(),
+            'listMapel' => $this->data_mapel_rapor($kelas->tingkat),
+            'totalMapel' => $this->total_mapel($kelas->tingkat),
+            'listJenis' => $listJenis
+        ];
+        return view('print.guru.print-ledger-pts', $data);
+    }
 }
---
+++ resources/js/Pages/Guru/PrintLedgerRapor.jsx
@@ -0,0 +1,80 @@
+import Kelas from '@/Components/Sia/Kelas'
+import PrintLink from '@/Components/Sia/PrintLink'
+import Semester from '@/Components/Sia/Semester'
+import Tahun from '@/Components/Sia/Tahun'
+import getDataKelasWaliKelas from '@/Functions/getDataKelasWaliKelas'
+import AppLayout from '@/Layouts/AppLayout'
+import { Head, useForm } from '@inertiajs/react'
+import React, { useEffect } from 'react'
+import { trackPromise } from 'react-promise-tracker'
+
+const PrintLedgerRapor = ({ initTahun, initSemester, listKelas }) => {
+
+    const { data, setData, errors } = useForm({
+        tahun: initTahun,
+        semester: initSemester,
+        kelasId: ''
+    })
+
+    const onHandleChange = (e) => {
+        setData(e.target.name, e.target.value)
+    }
+
+    async function getData() {
+        const response = await getDataKelasWaliKelas(data.tahun)
+        setData('kelasId', response.kelasId)
+    }
+
+    useEffect(() => {
+        if (data.tahun) {
+            trackPromise(
+                getData()
+            )
+        }
+    }, [data.tahun])
+
+    return (
+        <>
+            <Head title='Print Ledger Rapor' />
+            <div className="bg-emerald-200 border-b-2 border-emerald-500 font-bold text-center text-lg text-slate-600 uppercase mb-2">print ledger rapor</div>
+            <div className='lg:grid lg:grid-cols-4 lg:gap-2 lg:space-y-0 grid grid-cols-2 gap-2 pb-2'>
+                <Tahun
+                    id='tahun'
+                    name='tahun'
+                    value={data.tahun}
+                    message={errors.tahun}
+                    handleChange={onHandleChange}
+                />
+
+                <Semester
+                    id='semester'
+                    name='semester'
+                    value={data.semester}
+                    message={errors.semester}
+                    handleChange={onHandleChange}
+                />
+
+                <Kelas
+                    id='kelasId'
+                    name='kelasId'
+                    value={data.kelasId}
+                    message={errors.kelasId}
+                    handleChange={onHandleChange}
+                    listKelas={listKelas}
+                    disabled={true}
+                />
+                <div className="flex items-end">
+                    <PrintLink href={route('print-ledger-rapor.print', {
+                        tahun: data.tahun,
+                        semester: data.semester,
+                        kelasId: data.kelasId
+                    })} label='print' />
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+PrintLedgerRapor.layout = page => <AppLayout children={page} />
+export default PrintLedgerRapor

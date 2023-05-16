e6f9bf2234a3a35f725c82e724d6ea502e2b334e
taqiussa <taqiussa@gmail.com>
2023-05-16 20:11:07 +0700

    bimbingan detail

--- resources/js/Pages/Siswa/DetailBimbingan.jsx
+++ resources/js/Pages/Siswa/DetailBimbingan.jsx
@@ -10,10 +10,10 @@ const DetailBimbingan = ({ bimbingan }) => {
                 detail bimbingan dan konseling
             </div>
             <div className="lg:flex lg:justify-center">
-                <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row  hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
                     {
                         bimbingan.foto &&
-                        <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={`/storage/${bimbingan.foto}`} alt='foto' />
+                        <img className="md:object-fit object-contain px-1 w-full rounded-t-lg h-96 md:rounded-none md:rounded-l-lg" src={`/storage/${bimbingan.foto}`} alt='foto' />
                     }
                     <div className="flex flex-col justify-between p-4 leading-normal">
                         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{bimbingan.user?.name}</h5>
@@ -25,9 +25,12 @@ const DetailBimbingan = ({ bimbingan }) => {
             </div>
             {
                 bimbingan.foto_dokumen &&
-                <div className="flex justify-center my-5">
-                    <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={`/storage/${bimbingan.foto_dokumen}`} alt='foto dokumen' />
-                </div>
+                <>
+                    <div className="font-bold text-slate-600 text-lg mt-5">Foto Dokumen :</div>
+                    <div className="flex justify-center my-2 bg-white border border-gray-200 rounded-lg shadow md:flex-row  hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                        <img className="md:object-fit object-contain px-1 w-full rounded-t-lg h-96 md:rounded-none md:rounded-l-lg" src={`/storage/${bimbingan.foto_dokumen}`} alt='foto dokumen' />
+                    </div>
+                </>
             }
         </>
     )

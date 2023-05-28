4cd53cd57dea5f63cb30f55823f95c0447dc1e16
unknown <taqiussa>
2023-05-29 06:57:29 +0700

    ganti IP

--- app/Http/Middleware/IPRestrictionMiddleware.php
+++ app/Http/Middleware/IPRestrictionMiddleware.php
@@ -10,10 +10,10 @@ use Illuminate\Support\Str;
 class IPRestrictionMiddleware
 {
     // Define the allowed IP range
-    private $allowedIPRange = '36.71.82.';
-    private $allowedIPRangeTu = '36.71.85.';
+    private $allowedIPRange = '36.71.';
+    // private $allowedIPRangeTu = '36.71.85.';
     // private $allowedIPRange = '182.2.38.';  
-    private $allowedIPRangeLocal = '127.0.0.';  
+    private $allowedIPRangeLocal = '127.0.0.';
 
     /**
      * Handle an incoming request.
@@ -25,12 +25,14 @@ class IPRestrictionMiddleware
         $requestIP = $request->ip();
 
         // Check if the request IP matches the allowed IP range
-        if (Str::startsWith($requestIP, $this->allowedIPRange) 
-        || Str::startsWith($requestIP, $this->allowedIPRangeTu)
-         || Str::startsWith($requestIP, $this->allowedIPRangeLocal)) {
+        if (
+            Str::startsWith($requestIP, $this->allowedIPRange)
+            // || Str::startsWith($requestIP, $this->allowedIPRangeTu)
+            || Str::startsWith($requestIP, $this->allowedIPRangeLocal)
+        ) {
             return $next($request);
         }
-        
+
         abort(403, 'Terhubunglah dengan Wi-fi Ruang Guru Atau TU'); // Return 403 Forbidden if IP is not allowed
     }
 }

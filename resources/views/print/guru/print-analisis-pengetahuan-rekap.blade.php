86c2c531de4ceff682e54ef377e0315c3b752179
unknown <taqiussa>
2023-06-03 14:26:48 +0700

    print analisis nilai

--- resources/views/print/guru/print-analisis-pengetahuan-rekap.blade.php
+++ resources/views/print/guru/print-analisis-pengetahuan-rekap.blade.php
@@ -169,7 +169,7 @@
             <td class="py-1 px-2">&nbsp;</td>
             <td class="py-1 px-2">:</td>
             <td class="py-1 px-2">
-                {{ round($listSiswa->analisisPenilaian->where('nilai', '>=', $kkm)->count() / ($listSiswa->analisisPenilaian->where('nilai', '>=', $kkm)->count() + $listSiswa->analisisPenilaian->where('nilai', '<=', $kkm)), 2) }}
+                {{ round($listSiswa->analisisPenilaians->where('nilai', '>=', $kkm)->count() / ($listSiswa->analisisPenilaians->where('nilai', '>=', $kkm)->count() + $listSiswa->analisisPenilaians->where('nilai', '<=', $kkm)), 2) }}
                 x 100%</td>
         </tr>
         <tr>
@@ -177,7 +177,7 @@
             <td class="py-1 px-2">Daya serap soal</td>
             <td class="py-1 px-2">:</td>
             <td class="py-1 px-2">
-                {{ round($listSiswa->analisisPenilaian->where('nilai', '>=', $kkm)->count() / ($listSiswa->analisisPenilaian->where('nilai', '>=', $kkm)->count() + $listSiswa->analisisPenilaian->where('nilai', '<=', $kkm)), 2) * 100 }}
+                {{ round($listSiswa->analisisPenilaians->where('nilai', '>=', $kkm)->count() / ($listSiswa->analisisPenilaians->where('nilai', '>=', $kkm)->count() + $listSiswa->analisisPenilaians->where('nilai', '<=', $kkm)), 2) * 100 }}
                 %
             </td>
         </tr>
